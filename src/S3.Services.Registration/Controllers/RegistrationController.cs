81195bc4e5796ba0757a26ebba178e79e85bdbb3
Abeeb <abibcode@gmail.com>
2019-06-26 15:54:26 +0100

    Initial commit.

---
+++ src/S3.Services.Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using S3.Services.Registration.Models;
+
+namespace S3.Services.Registration.Controllers
+{
+    [Route("")]
+    public class RegistrationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Registration service running");
+    }
+}
