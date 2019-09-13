9e135d0fead9023941531449e000b98128ca040c
abibtj <abibcode@gmail.com>
2019-09-13 16:00:51 +0100

    Added_Modal_Dialog_UI

--- src/S3.Services.Registration/Classes/Queries/BrowseClassesQuery.cs
+++ src/S3.Services.Registration/Classes/Queries/BrowseClassesQuery.cs
@@ -1,3 +1,4 @@
+using System;
 using S3.Common.Types;
 using S3.Services.Registration.Dto;
 using System.Collections.Generic;
@@ -6,5 +7,6 @@ namespace S3.Services.Registration.Classes.Queries
 {
     public class BrowseClassesQuery : PagedQueryBase, IQuery<IEnumerable<ClassDto>>
     {
+        public Guid? SchoolId { get; set; }
     }
 }
